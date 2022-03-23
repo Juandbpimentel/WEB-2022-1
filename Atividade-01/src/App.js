9be243879f978aea126836c4286e9e8689ceb830
Juan Pimentel <juandbpimentel@alu.ufc.br>
2022-03-23 09:07:49 -0300

    Atividade 01

---
+++ Atividade-01/src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import Questao01 from './components/Questao01'
+import Questao02 from './components/Questao02'
+import Questao03 from './components/Questao03'
+import Questao04 from './components/Questao04'
+
+
+/* Questão 01 :
+function App(){
+  return(
+    <div className='App'>
+    <Questao01/>
+    </div>
+  )
+}
+
+*/
+
+/* Questão 02:
+function App(){
+  return(
+    <div className='App'>
+
+    <Questao02/>
+    </div>
+  )
+}
+*/
+
+/* Questão 03:
+function App(){
+  return(
+    <div className='App'>
+
+    <Questao03 nome='Juan David Bizerra Pimentel' curso='Engenharia de Computação' cidade='Rio de Janeiro'/>
+    </div>
+  );
+}
+*/
+
+function App(){
+  return(
+    <div className='App'>
+
+    <Questao04/>
+    </div>
+  );
+}
+
+export default App;
