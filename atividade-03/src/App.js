46d55104fd4381b2c6891cd45d0675243732d4f6
Juan Pimentel <juandbpimentel@alu.ufc.br>
2022-04-20 15:06:15 -0300

    Update App.js

--- atividade-03/src/App.js
+++ atividade-03/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
 import About from './components/pages/About';
