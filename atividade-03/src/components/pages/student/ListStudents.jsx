6842e96e29e7214a4770afee1d46fa8c8979e716
Juan David Bizerra Pimentel <61468560+Juandbpimentel@users.noreply.github.com>
2022-04-20 14:02:49 -0300

    Update ListStudents.jsx

--- atividade-03/src/components/pages/student/ListStudents.jsx
+++ atividade-03/src/components/pages/student/ListStudents.jsx
@@ -14,7 +14,7 @@ const ListStudents = () => {
     return (
         <div>
             <h4>Estudantes</h4>
-            <table className="table table-strict text-light">
+            <table className=""table table-striped" text-light">
                 <thead>
                     <tr>
                         <th>ID</th>
