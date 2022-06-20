6dc91f115190262dc371fc069259b4d8b5f7de82
Juan Pimentel <juandbpimentel@alu.ufc.br>
2022-06-20 09:12:01 -0300

    Organizando arquivos da atividade e criação da branch

--- atividades/atividade-05/crud-express-mongo/models/teacher/TeacherModel.js
+++
@@ -1,9 +0,0 @@
-module.exports = class TeacherModel {
-    constructor(_id, name, salary, university, degree) {
-        this._id = _id;
-        this.name = name;
-        this.salary = salary;
-        this.university = university;
-        this.degree = degree;
-    }
-};
